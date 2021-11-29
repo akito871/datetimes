13b36056b3c489a0b6a925148f12680b52ea17ef
akki-to <yanach1n919@gmail.com>
2021-11-30 01:48:18 +0900

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DataTimes />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DateTimes from './components/datetime.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DateTimes
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/datetime.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>{{date}}</p>
+        <p>{{time}}</p>
+    </div>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+
+export default {
+    data: function(){
+        return {
+            date: '',
+            time: ''
+        }
+    },
+    mounted: function(){
+        setInterval(this.updateTime,1000);
+    },
+    methods: {
+        updateTime: function(){
+            let current_date = dayjs()
+
+            this.date = current_date.format('YYYY') + '-' 
+                      + current_date.format('MM') + '-'
+                      + current_date.format('d')
+             
+            this.time = current_date.format('HH') + ':'
+                      + current_date.format('mm') + ':'
+                      + current_date.format('ss')
+        }
+    }
+}
+</script>
